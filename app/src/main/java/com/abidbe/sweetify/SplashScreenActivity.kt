1f545b4ef8422da00852243e0c8988af1f93e363
abidbe <abid.i.ft20@mail.umy.ac.id>
2024-05-26 18:45:40 +0700

    feat/splash-screen

---
+++ app/src/main/java/com/abidbe/sweetify/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.abidbe.sweetify
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.abidbe.sweetify.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iv.alpha = 0f
+        binding.iv.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+
+    }
+}

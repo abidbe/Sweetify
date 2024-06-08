4f98016cd68df2d2048bfe4b2db2bea13956995e
abidbe <abid.i.ft20@mail.umy.ac.id>
2024-06-08 07:30:59 +0700

    feat/onboarding-logingoogle

--- app/src/main/java/com/abidbe/sweetify/view/main/HomeFragment.kt
+++ app/src/main/java/com/abidbe/sweetify/view/main/HomeFragment.kt
@@ -1,14 +1,19 @@
 package com.abidbe.sweetify.view.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.abidbe.sweetify.databinding.FragmentHomeBinding
+import com.abidbe.sweetify.view.welcome.OnboardingActivity
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +21,28 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+        val firebaseUser = auth.currentUser
+        if (firebaseUser == null) {
+            // Not signed in, launch the Onboarding activity
+            startActivity(Intent(requireContext(), OnboardingActivity::class.java))
+            activity?.finish()
+            return
+        }
+        val displayName = firebaseUser.displayName
+        // Display user's profile information
+        binding.tvGreeting.text = "Hi, $displayName!"
+
+        firebaseUser.photoUrl?.let { photoUrl ->
+            // Use Glide to load the profile image
+            Glide.with(this)
+                .load(photoUrl)
+                .into(binding.imvUserPhoto)
+        }
     }
 
 }
---
+++ app/src/main/java/com/abidbe/sweetify/view/welcome/OnboardingActivity.kt
@@ -0,0 +1,177 @@
+package com.abidbe.sweetify.view.welcome
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
+import androidx.credentials.exceptions.GetCredentialException
+import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.widget.ViewPager2
+import com.abidbe.sweetify.R
+import com.abidbe.sweetify.view.main.MainActivity
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.Firebase
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.launch
+
+class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+    private lateinit var buttonNext: Button
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+
+        auth = Firebase.auth
+
+        viewPager = findViewById(R.id.viewPager)
+        tabLayout = findViewById(R.id.tabLayout)
+        buttonNext = findViewById(R.id.buttonNext)
+
+        val pages = listOf(
+            OnboardingPage(
+                R.drawable.undraw_donut_love,
+                getString(R.string.title_onboarding1),
+                getString(R.string.desc_onboarding1)
+            ),
+            OnboardingPage(
+                R.drawable.undraw_photo,
+                getString(R.string.title_onboarding2),
+                getString(R.string.desc_onboarding2)
+            ),
+            OnboardingPage(
+                R.drawable.undraw_report,
+                getString(R.string.title_onboarding3),
+                getString(R.string.desc_onboarding3)
+            )
+        )
+
+        val adapter = OnboardingAdapter(pages)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            // Tab configuration if needed
+        }.attach()
+
+        for (i in 0 until tabLayout.tabCount) {
+            val tab = (tabLayout.getChildAt(0) as ViewGroup).getChildAt(i)
+            val layoutParams = tab.layoutParams as ViewGroup.MarginLayoutParams
+            layoutParams.setMargins(10, 0, 10, 0)
+            tab.requestLayout()
+        }
+
+        buttonNext.setOnClickListener {
+            val currentItem = viewPager.currentItem
+            if (currentItem + 1 < pages.size) {
+                viewPager.currentItem = currentItem + 1
+            } else {
+                signIn()
+            }
+        }
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (position == pages.size - 1) {
+                    buttonNext.text = getString(R.string.login_google)
+                } else {
+                    buttonNext.text = getString(R.string.next)
+                }
+            }
+        })
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    private fun signIn() {
+        val credentialManager = CredentialManager.create(this)
+        val googleIdOption = GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(false)
+            .setServerClientId(getString(R.string.your_web_client_id))
+            .build()
+        val request = GetCredentialRequest.Builder()
+            .addCredentialOption(googleIdOption)
+            .build()
+
+        lifecycleScope.launch {
+            try {
+                val result: GetCredentialResponse = credentialManager.getCredential(
+                    request = request,
+                    context = this@OnboardingActivity,
+                )
+                handleSignIn(result)
+            } catch (e: GetCredentialException) {
+                Log.d("Error", e.message.toString())
+            }
+        }
+    }
+    private fun handleSignIn(result: GetCredentialResponse) {
+        when (val credential = result.credential) {
+            is CustomCredential -> {
+                if (credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+                    try {
+                        val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credential.data)
+                        firebaseAuthWithGoogle(googleIdTokenCredential.idToken)
+                    } catch (e: GoogleIdTokenParsingException) {
+                        Log.e(TAG, "Received an invalid google id token response", e)
+                    }
+                } else {
+                    Log.e(TAG, "Unexpected type of credential")
+                }
+            }
+
+            else -> {
+                Log.e(TAG, "Unexpected type of credential")
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential: AuthCredential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user: FirebaseUser? = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if (currentUser != null) {
+            startActivity(Intent(this@OnboardingActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+    companion object {
+        private const val TAG = "OnboardingActivity"
+    }
+}
